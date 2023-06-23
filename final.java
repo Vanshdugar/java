83f2fc8da7eb4cf2c5ec0cacc6a1744dd02cbd3d
Vansh Dugar <95927013+Vanshdugar@users.noreply.github.com>
2023-06-23 03:23:24 +0000

    npush

---
+++ final.java
@@ -0,0 +1,10 @@
+public class Final{
+    public static void main(String[] args){
+        int a=1;
+        while (a<=10) {
+            system.out.println(a);
+            a++;
+        }
+
+    }
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+public class Final{
+    public static void main (string args[]){
+        final int a=1;
+        while (a<=10){
+            system.out.println(a);
+            a++;
+        }
+
+    }
+}
